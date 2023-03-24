d2264cc805e2d9b4fd828ef84d13686a79e904da
8rxn <rajaryanvishwakarma10@gmail.com>
2023-03-24 22:55:07 +0530

    Add: Router Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
 import { useState } from "react";
 import "./App.css";
 import Button from "./components/shared/Button";
+import { Route,Routes } from "react-router";
+import { Clan, Friends, Home } from "./pages";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="bg-gradient-to-b from-bg-primary to-bg-secondary text-color-primary w-screen h-screen grid place-items-center">
+      <Routes>
+        <Route path="/" element={<><div className="bg-gradient-to-b from-bg-primary to-bg-secondary text-color-primary w-screen h-screen grid place-items-center">
         <h1>hello tribe</h1>
         <Button>Register</Button>
         <Button>Sign in</Button>
-      </div>
-    </>
+      </div></>}>
+        
+        </Route>
+
+        <Route path="/comrade" element={<Friends/>}/>
+        <Route path="/clan" element={<Clan/>}/>
+
+        {/* This will be default path when login will be setup */}
+        <Route path="/home" element={<Home/>}/>
+      </Routes>
+      </>
   );
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,3 @@
+export {default as Clan} from './Clan'
+export {default as Home} from './Home'
+export {default as Friends} from './Friends'
