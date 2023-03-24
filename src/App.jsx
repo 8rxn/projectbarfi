d7b14b9fb0e4bcf842e35500c9fb27e4d0c08ffc
SirSimon162 <anmolpattnayak2016@gmail.com>
2023-03-24 23:28:47 +0530

    add: images source links

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import Button from "./components/shared/Button";
-import { Route,Routes } from "react-router";
+import { Route, Routes } from "react-router";
 import { Clan, Friends, Home } from "./pages";
 
 function App() {
@@ -10,21 +10,25 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<><div className="bg-gradient-to-b from-bg-primary to-bg-secondary text-color-primary w-screen h-screen grid place-items-center">
-        <h1>hello tribe</h1>
-        <Button>Register</Button>
-        <Button>Sign in</Button>
-      </div></>}>
-        
-        </Route>
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="bg-gradient-to-b from-bg-primary to-bg-secondary text-color-primary w-screen h-screen grid place-items-center">
+                <h1>hello tribe</h1>
+                <Button>Register</Button>
+              </div>
+            </>
+          }
+        ></Route>
 
-        <Route path="/comrade" element={<Friends/>}/>
-        <Route path="/clan" element={<Clan/>}/>
+        <Route path="/comrade" element={<Friends />} />
+        <Route path="/clan" element={<Clan />} />
 
         {/* This will be default path when login will be setup */}
-        <Route path="/home" element={<Home/>}/>
+        <Route path="/home" element={<Home />} />
       </Routes>
-      </>
+    </>
   );
 }
 
---
+++ src/config/images.js
@@ -0,0 +1,40 @@
+export default {
+  assets: [
+    {
+      src: "https://res.cloudinary.com/dmofs5r4h/image/upload/v1679678867/Hack-36/Hero-main-05_j1p3il.png",
+      alt: "Hero-img",
+    },
+    {
+      src: "https://res.cloudinary.com/dmofs5r4h/image/upload/v1679678866/Hack-36/World-img-05_qg976f.png",
+      alt: "bg-img",
+    },
+    {
+      src: "https://res.cloudinary.com/dmofs5r4h/image/upload/v1679678866/Hack-36/Logo-05_bdvnst.png",
+      alt: "Logo",
+    },
+    {
+      src: "https://res.cloudinary.com/dmofs5r4h/image/upload/v1679678865/Hack-36/Features-1-05_lllonb.png",
+      alt: "Features-1",
+    },
+    {
+      src: "https://res.cloudinary.com/dmofs5r4h/image/upload/v1679678865/Hack-36/Features-2-05_sl1cgg.png",
+      alt: "Features-2",
+    },
+    {
+      src: "https://res.cloudinary.com/dmofs5r4h/image/upload/v1679678865/Hack-36/Features-3-05_uybexg.png",
+      alt: "Features-3",
+    },
+    {
+      src: "https://res.cloudinary.com/dmofs5r4h/image/upload/v1679678865/Hack-36/Features-4-05_erunkt.png",
+      alt: "Features-4",
+    },
+    {
+      src: "https://res.cloudinary.com/dmofs5r4h/image/upload/v1679678865/Hack-36/Game-PUBG-05_th4v0x.png",
+      alt: "PUBG-Games",
+    },
+    {
+      src: "https://res.cloudinary.com/dmofs5r4h/image/upload/v1679678865/Hack-36/FAQ-05_eh6l1x.png",
+      alt: "FAQ",
+    },
+  ],
+};
