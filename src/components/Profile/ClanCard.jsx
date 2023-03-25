f12aa9af4f8414d550285a7fa6ba7c7edf776cd4
SirSimon162 <anmolpattnayak2016@gmail.com>
2023-03-26 02:16:41 +0530

    fix: styles fixed in profile nfts and clans

--- src/components/Profile/ClanCard.jsx
+++ src/components/Profile/ClanCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Card({text}) {
   return (
-    <div className='flex flex-col sm:flex-row w-[100%] md:w-[80%] xl:w-[220px] items-center justify-between gap-3 bg-bg-primary border-color-primary border-[2px] py-2 px-3 rounded-tr-xl rounded-bl-xl flex-shrink-0'>
+    <div className='flex flex-col sm:flex-row w-[100%] md:w-[80%] xl:w-[220px] items-start sm:items-center justify-between gap-3 bg-bg-primary border-color-primary border-[2px] py-2 px-2 rounded-tr-xl rounded-bl-xl flex-shrink-0'>
         <p className='text-base lg:text-xl font-[500] text-color-primary'>{text}</p>
         <button className='text-color-primary rounded-xl bg-bg-secondary py-1 px-2 border-[1px] border-color-primary'>View Clan</button>
     </div>
