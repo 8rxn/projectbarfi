48ff60a9a2e4bd5431f974b091cf807803dcca67
8rxn <rajaryanvishwakarma10@gmail.com>
2023-03-25 14:23:59 +0530

    Add: Arcana Setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import "./App.css";
 import { Route, Routes } from "react-router";
 import { Clan, Friends, Home } from "./pages";
 import NavBar from "../src/components/marginals/NavBar/NavBar";
+import { useAuth,Auth } from '@arcana/auth-react';
 import Profile from "./components/Profile/Profile";
 
 function App() {
+
+  const onLogin = () => {
+    // Route to authenticated page
+  }
   const [count, setCount] = useState(0);
   const [profileActive, setProfileActive] = useState(false);
-
+  const auth = useAuth();
   const toggleProfile= useCallback(
     () => {
       setProfileActive(!profileActive)
@@ -23,6 +28,18 @@ function App() {
       <div className={` h-screen z-10 fixed left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%] grid place-items-center w-[100vw] shadow dark:bg-white/5 ${!profileActive?"hidden":""} `}>
       <Profile toggleProfile={toggleProfile}/>
       </div>
+
+      {/* <div className="fixed top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] bg-blend-darken dark:bg-blur-3xl z-10 ">
+      {auth.loading ? (
+        "Loading"
+      ) : auth.isLoggedIn ? (
+        <p>Logged In</p>
+      ) : (
+        <div>
+          <Auth externalWallet={true} theme={"dark"} onLogin={onLogin}/>
+        </div>
+      )}
+    </div> */}
 
       <Routes>
         <Route path="/" element={<Home />}></Route>
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import './index.css'
+import { AuthProvider, CHAIN } from "@arcana/auth";
+import { ProvideAuth } from "@arcana/auth-react";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const appAddress = "xar_test_424ee9f5647103607d891d487c73c9f794188a2d";
+const auth = new AuthProvider(`${appAddress}`, {
+  network: "testnet",
+  position: "right",
+  theme: "dark",
+  alwaysVisible: false,
+  chainConfig: {
+    chainId: CHAIN.POLYGON_MUMBAI_TESTNET,
+    rpcUrl:
+      "https://fragrant-greatest-sky.matic-testnet.discover.quiknode.pro/51a324530a248242144de9b79fc28e6f2272e30d/",
+  },
+});
+
+async function initialise() {
+  await auth.init();
+  console.log("initialise");
+}
+initialise();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+    <ProvideAuth provider={auth}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ProvideAuth>
+  </React.StrictMode>
+);
