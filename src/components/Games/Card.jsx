cdd94e0c0e9b4f43d81b8a01953b12e935ae6d3b
SirSimon162 <anmolpattnayak2016@gmail.com>
2023-03-25 20:26:47 +0530

    add: games added

--- src/components/Games/Card.jsx
+++ src/components/Games/Card.jsx
@@ -5,7 +5,7 @@ function Card({src, alt, text}) {
   return (
     <div className='flex flex-col items-center justify-center gap-3 bg-bg-primary border-color-primary border-[2px] p-5'>
         <div className='w-full'>
-            <img src={src} alt={alt} text={text} className="w-48 border-white border-[1px] min-w-48"/>
+            <img src={src} alt={alt} text={text} className="w-48 h-48 border-white border-[1px] min-w-48 object-cover"/>
         </div>
         <h3 className='text-xl font-[500]'>{text}</h3>
         <Button>Join Clan</Button>
