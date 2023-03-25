8694953a64a5d52756bb6b9c65fa8a64fdb78336
SirSimon162 <anmolpattnayak2016@gmail.com>
2023-03-25 20:39:31 +0530

    fix: styling issues in games fixed

--- src/components/Games/Card.jsx
+++ src/components/Games/Card.jsx
@@ -3,9 +3,9 @@ import Button from '../shared/Button'
 
 function Card({src, alt, text}) {
   return (
-    <div className='flex flex-col items-center justify-center gap-3 bg-bg-primary border-color-primary border-[2px] p-5'>
+    <div className='flex flex-col items-center justify-center gap-3 bg-bg-primary border-color-primary border-[2px] p-5 min-w-fit'>
         <div className='w-full'>
-            <img src={src} alt={alt} text={text} className="w-48 h-48 border-white border-[1px] min-w-48 object-cover"/>
+            <img src={src} alt={alt} text={text} className="w-44 h-48 border-white border-[1px] object-cover"/>
         </div>
         <h3 className='text-xl font-[500]'>{text}</h3>
         <Button>Join Clan</Button>
