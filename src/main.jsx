c07bebaf10316218ee70192af764151f1d017a56
Anushrey Jain <anushrxy@gmail.com>
2023-03-25 17:41:29 +0530

    Arcana basic setup

--- src/main.jsx
+++ src/main.jsx
@@ -6,16 +6,16 @@ import { AuthProvider, CHAIN } from "@arcana/auth";
 import { ProvideAuth } from "@arcana/auth-react";
 import "./index.css";
 
-const appAddress = "xar_test_424ee9f5647103607d891d487c73c9f794188a2d";
+const appAddress = "xar_test_c7d67b8c243fc2be269f188ce225ce18472e1dc4";
 const auth = new AuthProvider(`${appAddress}`, {
   network: "testnet",
   position: "right",
   theme: "dark",
   alwaysVisible: false,
   chainConfig: {
-    chainId: CHAIN.POLYGON_MUMBAI_TESTNET,
+    chainId: CHAIN.ETHEREUM_GOERLI,
     rpcUrl:
-      "https://fragrant-greatest-sky.matic-testnet.discover.quiknode.pro/51a324530a248242144de9b79fc28e6f2272e30d/",
+      "https://eth-goerli.g.alchemy.com/v2/mzbwFJs7hwpG5epgv_VYc1r46QzPtrh4",
   },
 });
 
